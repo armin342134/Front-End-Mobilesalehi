a32311f2d66ab5cb988550e2c6c5b5ec0d8bee80
armin <armyn3421@gmail.com>
2024-12-16 12:26:34 +0330

    update images

--- src/components/ProductsDetail.jsx
+++ src/components/ProductsDetail.jsx
@@ -76,7 +76,7 @@ export default function ProductsDetail() {
                   style={{
                     objectFit: "cover",
                     width: "100%",
-                    height: "100%",
+                    height: "90%",
                   }}
                 />
               </SwiperSlide>
